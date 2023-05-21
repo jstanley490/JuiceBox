48a8760049ae58f45014d187df710cc4fc597c73
jstanley490 <jstanley490@gmail.com>
2023-05-21 15:18:00 -0500

    end of part 3

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const tagsRouter = express.Router();
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -15,6 +15,36 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  try {
+    const tagName = req.params.tagName;
+    // use our method to get posts by tag name from the db
+    const allPosts = await getPostsByTagName(tagName);
+    const posts = allPosts.filter((post) => {
+      // keep a post if it is either active, or if it belongs to the current user
+      // the post is active, doesn't matter who it belongs to
+      if (post.active) {
+        return true;
+      }
+
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+
+      // none of the above are true
+      return false;
+    });
+
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -218,6 +218,13 @@ async function getPostById(postId) {
       [postId]
     );
 
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId",
+      };
+    }
+
     const { rows: tags } = await client.query(
       `
       SELECT tags.*
@@ -383,6 +390,7 @@ module.exports = {
   createPost,
   updatePost,
   getAllPosts,
+  getPostById,
   getPostsByUser,
   getPostsByTagName,
   createTags,

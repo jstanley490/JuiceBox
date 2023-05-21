3b898fe0521fe86712fa67fa2e34ee3c3b482e2f
jstanley490 <jstanley490@gmail.com>
2023-05-21 16:06:54 -0500

    update .env for db source

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
-const PORT = 3000;
+// const PORT = 3000;
 const express = require("express");
 const server = express();
 const morgan = require("morgan");
 server.use(morgan("dev"));
+
+const { client } = require("./db");
+
+client.connect();
 
 // server.get("/background/:color", (req, res, next) => {
 //   res.send(`
@@ -25,9 +29,6 @@ server.use(express.json());
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
 
-const { client } = require("./db");
-client.connect();
-
-server.listen(PORT, () => {
-  console.log("The server is up on port", PORT);
-});
+// server.listen(PORT, () => {
+//   console.log("The server is up on port", PORT);
+// });
